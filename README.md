c7120ec5b1c13a29dd54eeda159a4dc429f37968
xiaogouxo <skylzl001@163.com>
2015-04-13 18:20:04 +0800

    init README.md

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # baasbox-php
+A simple PHP client for [Baasbox](https://github.com/baasbox/baasbox).
+Inspired by [hook-php](https://github.com/doubleleft/hook-php).
+
+# Usage
+---
+
+```php
+<?php
+$box = Baasbox\Client::configure(array(
+  'app_id' => 1234567890,
+  'endpoint' => 'http://www.test.com:9000/',
+  'authorization'=>'fdc:123456'	
+));
+
+$box->collection('scores')->create(array(
+  'name' => 'Endel',
+  'score' => 7
+));
+```
---
+++ example/baasbox.class.php
@@ -0,0 +1,93 @@
+<?php
+
+class baasbox{
+
+    public $table;			// table name
+	/**
+	 * Init baasbox conf
+	 * @param string $var
+	 * @return object
+	 */
+	function __get($var) {
+		switch ($var) {
+			case 'box':
+				return $this->load_baasbox();
+            case 'box_conf':
+                return $this->box_conf = &$_ENV['_config']['baasbox'];
+		}
+	}
+
+	/**
+	 * magic method
+	 * @param string $method
+	 */
+	function __call($method, $args) {
+		throw new Exception("$method is not existed");
+	}
+
+	/**
+	 * load baasbox instance
+	 * @return object
+	 */
+	public function load_baasbox() {
+        $baasbox = Hook\Client::getInstance();
+        if(isset($baasbox)) {
+            return $baasbox;
+        }else{
+            return Hook\Client::configure(array(
+			  'app_id' => 1234567890,
+			  'endpoint' => 'http://www.test.com:9000/',
+			  'authorization'=>'test:123456'	
+			));
+        }
+	}
+
+    public function find($key) {
+        return $this->box->collection($this->table)->find($key);
+    }
+
+    public function create($data=array()) {
+        return $this->box->collection($this->table)->create($data);
+    }
+
+    public function get(){
+        return $this->box->collection($this->table)->get();
+    }
+
+    public function where($field, $_operation = null, $_value = null, $opera = 'and'){
+        return $this->box->collection($this->table)->where($field, $_operation, $_value, $opera);
+    }
+
+    public function orWhere($field, $_operation = null, $_value = null){
+        return $this->box->collection($this->table)->where($field, $_operation, $_value, 'or');
+    }
+
+    public function count() {
+        return $this->box->collection($this->table)->count();
+    }
+
+    public function orderBy($field, $direction = null){
+        return $this->box->collection($this->table)->orderBy($field, $direction = null);
+    }
+
+    public function recordsPerPage($int){
+        return $this->box->collection($this->table)->recordsPerPage($int);
+    }
+
+    public function page($int){
+        return $this->box->collection($this->table)->page($int);
+    }
+
+    public function skip($int){
+        return $this->box->collection($this->table)->skip($int);
+    }
+
+    public function delete($_id = null){
+        return $this->box->collection($this->table)->delete($_id);
+    }
+
+    public function update($_id, array $data = null){
+        return $this->box->collection($this->table)->update($_id,$data);
+    }
+
+}
